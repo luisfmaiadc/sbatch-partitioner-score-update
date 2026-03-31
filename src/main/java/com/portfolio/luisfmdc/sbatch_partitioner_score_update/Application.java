637115b71ef3934c7d80e793f273d8b0f773cc0f
Luis Felipe Maia <lf.mcosta23@gmail.com>
2026-03-30 22:54:33 -0300

    Estrutura base do projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/sbatch_partitioner_score_update/Application.java
@@ -0,0 +1,11 @@
+package com.portfolio.luisfmdc.sbatch_partitioner_score_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
